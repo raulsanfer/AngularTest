b9140fabbcd140a5631ec33fadbcfdf683e0b335
Raúl Sánchez Fernández <sanchezraul77@gmail.com>
2025-06-22 20:25:05 +0200

    aplicando pipe para json

--- 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { JsonpClientBackend } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent,JsonPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent { 
+//test jsonpipe
+
+  jsonData = {
+    name: 'John Doe',
+    age: 30,
+    city: 'New York',
+    hobbies: ['reading', 'traveling', 'gaming'],
+    isActive: true,
+    address: {
+      street: '123 Main St',
+      zipCode: '10001'
+    }
+  };
+  
+  //var showJson = signal();
+
+    getHobbiesStringOnly() {
+    return this.jsonData.hobbies.join(', ');
+  } 
+  
+  getHobbiesJson() {
+    return this.jsonData.hobbies;
+  } 
+  getAddressJson() {
+    return this.jsonData.address;
+  }
+}
